49281bd4b3f79ff19a6af41c4929a311eb5236a0
Austin Rutherford <austinrutherford@gmail.com>
2024-11-17 22:37:14 -0600

    Update provision.tf
    added:
     dynamic lookup for ubuntu image by region
     updated keyname and tags to have a unique identifier easily stored
     spot instance parameter (default "no")

--- terraform/provision.tf
+++ terraform/provision.tf
@@ -1,86 +1,78 @@
 provider "aws" {
-	region = "us-east-2"
-}
-
-variable "scylla_instance_type" {
-	default = "i4i.8xlarge"
-}
-
-variable "loader_instance_type" {
-	default = "c7i.12xlarge"
+  region  = var.aws_region
+  profile = var.aws_profile
 }
 
 resource "tls_private_key" "tablets_demo" {
-	algorithm = "RSA"
-	rsa_bits = 4096
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
-# Hopefully you won't ever need to configure anything below here.
 resource "aws_vpc" "tablets_demo" {
-	cidr_block = "172.31.0.0/16"
-	enable_dns_support = true
-	enable_dns_hostnames = true
+  cidr_block              = "172.31.0.0/16"
+  enable_dns_support      = true
+  enable_dns_hostnames    = true
 
-	tags = {
-		Name = "tablets_demo"
-	}
+  tags = {
+    Name = "${var.unique_identifier}_tablets_demo"
+  }
 }
 
 resource "aws_internet_gateway" "tablets_demo_igw" {
-	vpc_id = aws_vpc.tablets_demo.id
+  vpc_id = aws_vpc.tablets_demo.id
 
-	tags = {
-		Name = "tablets_demo_igw"
-	}
+  tags = {
+    Name = "${var.unique_identifier}_tablets_demo_igw"
+  }
 }
 
 resource "aws_subnet" "tablets_demo_subnet" {
-	vpc_id = aws_vpc.tablets_demo.id
-	cidr_block = "172.31.0.0/16"
-	map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.tablets_demo.id
+  cidr_block              = "172.31.0.0/16"
+  map_public_ip_on_launch = true
 
-	tags = {
-		Name = "tablets_demo_subnet"
-	}
+  tags = {
+    Name = "${var.unique_identifier}_tablets_demo_subnet"
+  }
 }
 
 resource "aws_route_table" "tablets_demo_public_rt" {
-	vpc_id = aws_vpc.tablets_demo.id
-	tags = {
-		Name = "tablets_demo_public_rt"
-	}
+  vpc_id = aws_vpc.tablets_demo.id
+
+  tags = {
+    Name = "${var.unique_identifier}_tablets_demo_public_rt"
+  }
 }
 
 resource "aws_route" "public_route" {
-	route_table_id = aws_route_table.tablets_demo_public_rt.id
-	destination_cidr_block = "0.0.0.0/0"
-	gateway_id = aws_internet_gateway.tablets_demo_igw.id
+  route_table_id         = aws_route_table.tablets_demo_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.tablets_demo_igw.id
 }
 
 resource "aws_route_table_association" "tablets_demo_public_rt_assoc" {
-	subnet_id = aws_subnet.tablets_demo_subnet.id
-	route_table_id = aws_route_table.tablets_demo_public_rt.id
+  subnet_id      = aws_subnet.tablets_demo_subnet.id
+  route_table_id = aws_route_table.tablets_demo_public_rt.id
 }
 
 resource "aws_security_group" "tablets_demo_sg" {
-	vpc_id = aws_vpc.tablets_demo.id
-	name = "tablets_demo_sg"
+  vpc_id = aws_vpc.tablets_demo.id
+  name   = "${var.unique_identifier}_tablets_demo_sg"
 
-	ingress {
-		from_port = 0
-		to_port = 0
-		protocol = "-1"
-		cidr_blocks = [aws_vpc.tablets_demo.cidr_block] # Internal ingress
-	}
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.tablets_demo.cidr_block] # Internal ingress
+  }
 
-	egress {
-		from_port = 0
-		to_port = 0
-		protocol = "-1"
-		cidr_blocks = [aws_vpc.tablets_demo.cidr_block] # Internal egress
-	}
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.tablets_demo.cidr_block] # Internal egress
+  }
 
-	# Public Internet
   ingress {
     from_port   = 3000
     to_port     = 3000
@@ -116,7 +108,6 @@ resource "aws_security_group" "tablets_demo_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow all outbound traffic to external networks
   egress {
     from_port   = 0
     to_port     = 0
@@ -124,95 +115,137 @@ resource "aws_security_group" "tablets_demo_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-	tags = {
-		Name = "tablets_demo_sg"
-	}
+  tags = {
+    Name = "${var.unique_identifier}_tablets_demo_sg"
+  }
 }
+
 resource "aws_key_pair" "kp" {
-	key_name = "tablets-demo" # Ideally, replace it with something unique to you
-	public_key = tls_private_key.tablets_demo.public_key_openssh
-
+  key_name   = "${var.unique_identifier}_tablets_demo"
+  public_key = tls_private_key.tablets_demo.public_key_openssh
 }
 
-resource "aws_placement_group" "tablets_demo" {
-  name     = "tablets_demo"
+resource "aws_placement_group" "tablets_demo_pg" {
+  name     = "${var.unique_identifier}_tablets_demo_pg"
   strategy = "cluster"
 
   tags = {
-    Name = "tablets_demo"
+    Name = "${var.unique_identifier}_tablets_demo_pg"
   }
 }
 
 resource "local_file" "pem_file" {
-	filename = pathexpand("./tablets_demo.pem")
-	file_permission = "600"
-	sensitive_content = tls_private_key.tablets_demo.private_key_pem
+  filename         = pathexpand("./${var.unique_identifier}_tablets_demo.pem")
+  file_permission  = "600"
+  sensitive_content = tls_private_key.tablets_demo.private_key_pem
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical's official AWS account ID
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 resource "aws_instance" "scylladb_node" {
-	count = 6
-	ami = "ami-00eb69d236edcfaf8" # https://cloud-images.ubuntu.com/locator/ec2/
-	instance_type = var.scylla_instance_type
-	key_name = aws_key_pair.kp.key_name
-	user_data = file("${path.module}/scylla-bootstrap.sh")
+  count         = 6
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.scylla_instance_type
+  key_name      = aws_key_pair.kp.key_name
+  user_data     = file("${path.module}/scylla-bootstrap.sh")
 
-	private_ip = "172.31.0.10${count.index + 1}"
+  private_ip = "172.31.0.10${count.index + 1}"
 
-	subnet_id = aws_subnet.tablets_demo_subnet.id
-	vpc_security_group_ids = [aws_security_group.tablets_demo_sg.id]
-	placement_group = aws_placement_group.tablets_demo.name
+  subnet_id              = aws_subnet.tablets_demo_subnet.id
+  vpc_security_group_ids = [aws_security_group.tablets_demo_sg.id]
+  placement_group        = aws_placement_group.tablets_demo_pg.name
 
-	root_block_device {
-		volume_size = 64 #GB
-		volume_type = "gp3"
-	}
+  root_block_device {
+    volume_size = 64 #GB
+    volume_type = "gp3"
+  }
 
-	tags = {
-		Name = "scylladb-node-${count.index + 1}"
-	}
+  dynamic "instance_market_options" {
+    for_each = var.use_spot_instances == "yes" ? [1] : []
+    content {
+      market_type = "spot"
+      spot_options {
+        max_price = "0.5"
+      }
+    }
+  }
 
+  tags = {
+    Name = "${var.unique_identifier}_scylladb-node-${count.index + 1}"
+  }
 }
 
 resource "aws_instance" "loader_node" {
-	count = 3
-	ami = "ami-00eb69d236edcfaf8"
-	instance_type = var.loader_instance_type
-	key_name = aws_key_pair.kp.key_name
-	user_data = file("${path.module}/loader_monitoring_setup.sh")
+  count         = 3
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.loader_instance_type
+  key_name      = aws_key_pair.kp.key_name
+  user_data     = file("${path.module}/loader_monitoring_setup.sh")
 
-	private_ip = "172.31.0.20${count.index + 1}"
-        subnet_id = aws_subnet.tablets_demo_subnet.id
-        vpc_security_group_ids = [aws_security_group.tablets_demo_sg.id]
-        placement_group = aws_placement_group.tablets_demo.name
-         
-        root_block_device {
-                volume_size = 64 #GB
-                volume_type = "gp3"
-        }       
-        
-        tags = {
-                Name = "loader-node-${count.index + 1}"
-        }
+  private_ip = "172.31.0.20${count.index + 1}"
+  subnet_id  = aws_subnet.tablets_demo_subnet.id
+  vpc_security_group_ids = [aws_security_group.tablets_demo_sg.id]
+  placement_group        = aws_placement_group.tablets_demo_pg.name
 
-        connection {
-                host = "${self.public_ip}"
-                type = "ssh"
-                user = "ubuntu"
-                #private_key = file("${path.module}/tablets_demo.pem")
-		private_key = tls_private_key.tablets_demo.private_key_pem
-        }   
+  root_block_device {
+    volume_size = 64 #GB
+    volume_type = "gp3"
+  }
 
+  dynamic "instance_market_options" {
+    for_each = var.use_spot_instances == "yes" ? [1] : []
+    content {
+      market_type = "spot"
+      spot_options {
+        max_price = "0.5"
+      }
+    }
+  }
 
-	provisioner "file" {
-		source = "${path.module}/tablets_demo.pem"
-		destination = "/home/ubuntu/.ssh/id_rsa"
-	}
+  tags = {
+    Name = "${var.unique_identifier}_loader-node-${count.index + 1}"
+  }
 
-	provisioner "remote-exec" {
-		inline = [
-		  "chmod 700 /home/ubuntu/.ssh",
-                  "chown -R ubuntu:ubuntu /home/ubuntu",
-                  "chmod 600 /home/ubuntu/.ssh/id_rsa"
-                ]
-        }
+  connection {
+    host        = self.public_ip
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = tls_private_key.tablets_demo.private_key_pem
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/${var.unique_identifier}_tablets_demo.pem"
+    destination = "/home/ubuntu/.ssh/id_rsa"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod 700 /home/ubuntu/.ssh",
+      "chown -R ubuntu:ubuntu /home/ubuntu",
+      "chmod 600 /home/ubuntu/.ssh/id_rsa"
+    ]
+  }
 }
