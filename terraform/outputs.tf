c7478802f462d54f3d9488eb3f3c3d370417447b
Austin Rutherford <austinrutherford@gmail.com>
2024-11-18 13:04:02 -0600

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,7 @@
-
-
-# Output for ScyllaDB Nodes
-output "scylla_nodes_public_ips" {
-  description = "Public IPs of ScyllaDB nodes"
-  value       = [for instance in aws_instance.scylladb_node : instance.public_ip]
-}
-
-output "scylla_nodes_private_ips" {
-  description = "Private IPs of ScyllaDB nodes"
-  value       = [for instance in aws_instance.scylladb_node : instance.private_ip]
+# Output for unique_identifier
+output "unique_identifier" {
+  description = "Unique identifier for the deployment"
+  value       = var.unique_identifier
 }
 
 output "scylla_nodes_public_dns" {
@@ -19,16 +12,6 @@ output "scylla_nodes_public_dns" {
 output "demo_portal" {
   value = format("http://%s:5000", aws_instance.loader_node[0].public_dns)
   description = "URL of the Demo Portal"
-}
-# Output for Loader Nodes
-output "loader_nodes_public_ips" {
-  description = "Public IPs of Loader nodes"
-  value       = [for instance in aws_instance.loader_node : instance.public_ip]
-}
-
-output "loader_nodes_private_ips" {
-  description = "Private IPs of Loader nodes"
-  value       = [for instance in aws_instance.loader_node : instance.private_ip]
 }
 
 output "loader_nodes_public_dns" {
